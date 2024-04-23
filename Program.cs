ec3a72feb1dc1ae8f73df36a724d0fc1d15168a6
Alin Burghila <108270121+burghila@users.noreply.github.com>
2024-04-23 20:52:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
+            Console.WriteLine("Enter a number to divide each element by:");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int number in numbers)
+            {
+                int result = number / divisor;
+                Console.WriteLine($"Result of {number} / {divisor} is {result}");
+            }
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please enter a valid integer.");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Division by zero is not allowed.");
+        }
+        finally
+        {
+            Console.WriteLine("The program has completed execution.");
+        }
+    }
+}
